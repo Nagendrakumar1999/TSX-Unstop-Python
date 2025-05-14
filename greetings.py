71f1c3cb0921cc27187f82cd1f866bc5e4f4a589
Nagendrakumar1999 <NAGENDRAKUMARNANI2@GMAIL.COM>
2025-05-14 08:58:46 +0530

    Pycharm

---
+++ greetings.py
@@ -0,0 +1,13 @@
+
+# greeting.py
+
+# This script prints the user's name and a greeting message
+
+# Define your name as a string
+name =input('enter the name')  # This prompts the user to enter their name
+
+# Print a simple greeting message
+print("Hello!")  # This prints a general greeting
+
+# Print a personalized greeting using the name variable
+print("Welcome,", name)  # This prints your name along with a welcome message
